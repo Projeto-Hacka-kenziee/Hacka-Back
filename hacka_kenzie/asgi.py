ee9f055174af7df635ba7141726595d09590e65b
Jalles <jallessantos12@gmail.com>
2023-11-20 17:17:52 -0300

    feat: added poetry support to manage packages

--- hacka_kenzie/asgi.py
+++ hacka_kenzie/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for hackakenzie project.
+ASGI config for hacka_kenzie project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackakenzie.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hacka_kenzie.settings")
 
 application = get_asgi_application()
--- hacka_kenzie/wsgi.py
+++ hacka_kenzie/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hackakenzie project.
+WSGI config for hacka_kenzie project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackakenzie.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hacka_kenzie.settings")
 
 application = get_wsgi_application()
